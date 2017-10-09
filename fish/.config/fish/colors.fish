6e5d50dea52ee7103cf77108c7bffa067b05c307
Adrien Bak <adrien.bak@rakuten.com>
2017-10-09 13:41:47 +0200

    readline colors

---
+++ fish/.config/fish/colors.fish
@@ -0,0 +1,24 @@
+# Readline colors
+set -g fish_color_autosuggestion 555 yellow
+set -g fish_color_command 5f87d7
+set -g fish_color_comment 808080
+set -g fish_color_cwd 87af5f
+set -g fish_color_cwd_root 5f0000
+set -g fish_color_error 870000 --bold
+set -g fish_color_escape af5f5f
+set -g fish_color_history_current 87afd7
+set -g fish_color_host 5f87af
+set -g fish_color_match d7d7d7 --background=303030
+set -g fish_color_normal normal
+set -g fish_color_operator d7d7d7
+set -g fish_color_param 5f87af
+set -g fish_color_quote d7af5f
+set -g fish_color_redirection normal
+set -g fish_color_search_match --background=purple
+set -g fish_color_status 5f0000
+set -g fish_color_user 5f875f
+set -g fish_color_valid_path --underline
+
+set -g fish_color_dimmed 555
+set -g fish_color_separator 999
+
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@
 set current_dir (cd (dirname (readlink -m (status -f))); and pwd)
 set -x DOTFILES (dirname (dirname (dirname $current_dir)))
 
+#let's not source that for the time being
+#source colors.fish
+
 # PURE prompt 
 source pure_config.fish
 
@@ -10,3 +13,4 @@ source $HOME/.config/fish/functions/z-fish/z.fish
 
 # Set my paths and stuff
 source env.fish
+
