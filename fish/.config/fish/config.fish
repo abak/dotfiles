44f2bd035c6e41bb2336e5b9f08aa7292de86c8e
Adrien Bak <adrien.bak@rakuten.com>
2017-10-09 12:42:33 +0200

    factor fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,20 +2,11 @@
 set current_dir (cd (dirname (readlink -m (status -f))); and pwd)
 set -x DOTFILES (dirname (dirname (dirname $current_dir)))
 
-
-# THEME PURE #
-set fish_function_path $HOME/.config/fish/functions/pure $fish_function_path
-
-set pure_color_blue (set_color -o blue)
-set pure_color_cyan (set_color "1e95fd")
-set pure_color_gray (set_color -o "6c6c6c")
-set pure_color_green (set_color "66ff66")
-set pure_color_normal (set_color "000000")
-set pure_color_red (set_color "f820ff")
-set pure_color_yellow (set_color "1bc8c8")
+# PURE prompt 
+source pure_config.fish
 
 # Z
 source $HOME/.config/fish/functions/z-fish/z.fish
 
-
+# Set my paths and stuff
 source env.fish
---
+++ fish/.config/fish/pure_config.fish
@@ -0,0 +1,10 @@
+set fish_function_path $HOME/.config/fish/functions/pure $fish_function_path
+
+set pure_color_blue (set_color -o blue)
+set pure_color_cyan (set_color "1e95fd")
+set pure_color_gray (set_color -o "6c6c6c")
+set pure_color_green (set_color "66ff66")
+set pure_color_normal (set_color "000000")
+set pure_color_red (set_color "f820ff")
+set pure_color_yellow (set_color "1bc8c8")
+
