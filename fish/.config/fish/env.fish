5e065db0b84c9f30ddfba1f32541e4ce818b8bef
Adrien Bak <adrien.bak@gmail.com>
2020-10-11 16:02:52 +0200

    adds brew-specific path to path

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -1,6 +1,10 @@
 set PATH $HOME/bin $PATH
 
 set PATH $HOME/scripts $PATH
+set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
+
+# this is terrible
+set PATH /usr/local/opt/python/libexec/bin $PATH
 
 set -Ux EDITOR vim
 
