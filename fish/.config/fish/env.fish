8369e228530c849d3475ff889b4d4de1d48a4a26
Adrien Bak <adrien.bak@gmail.com>
2021-12-23 16:14:07 +0100

    update python path for brew

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -4,9 +4,11 @@ set PATH $HOME/scripts $PATH
 set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 
 # this is terrible
-set PATH /usr/local/opt/python/libexec/bin $PATH
+set PATH /usr/local/opt/python@3.8/libexec/bin $PATH
 
 set -Ux EDITOR vim
 
 set -Ux VIRTUAL_ENV_DISABLE_PROMPT 1
 
+set PATH $HOME/.cargo/bin $PATH
+set JAVA_HOME /usr/bin
