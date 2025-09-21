fc2b414052af7174ff06885301d9a10885664d58
Adrien Bak <adrien.bak@gmail.com>
2025-09-21 19:48:44 +0200

    fix order of includes in config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,19 +4,22 @@ if [ $uname = "Darwin" ]
   set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
 end
 
-set current_dir (cd (dirname (readlink -m (status -f))); and pwd)
+set fish_greeting
+
+set current_dir (cd (dirname (greadlink -m (status -f))); and pwd)
 set -x DOTFILES (dirname (dirname (dirname $current_dir)))
 
 set config_path $HOME/.config/fish
 
 source $config_path/colors.fish
 
+# Set my paths and stuff
+source $config_path/env.fish
 
 # Z
 #source $config_path/functions/z-fish/z.fish
 
-# Set my paths and stuff
-source $config_path/env.fish
+
 
 if test -e $config_path/local.fish
   source $config_path/local.fish
